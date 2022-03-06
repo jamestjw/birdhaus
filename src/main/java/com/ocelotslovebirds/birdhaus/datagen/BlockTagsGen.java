77d0e0662b064664eb689c7759b655d3e1cc060c
shafiq.imtiaz <shafiqimtiaz@gmail.com>
2022-03-06 15:55:08 -0500

    add global tagging - to enable spawing birdhouse using any type of plank OR logs

--- src/main/java/com/ocelotslovebirds/birdhaus/datagen/BlockTagsGen.java
+++ src/main/java/com/ocelotslovebirds/birdhaus/datagen/BlockTagsGen.java
@@ -4,6 +4,8 @@ import com.ocelotslovebirds.birdhaus.Core;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class BlockTagsGen extends BlockTagsProvider {
@@ -23,5 +25,17 @@ public class BlockTagsGen extends BlockTagsProvider {
      */
     @Override
     protected void addTags() {
+        tag(BlockTags.PLANKS)
+                .add(Blocks.OAK_PLANKS, Blocks.SPRUCE_PLANKS, Blocks.BIRCH_PLANKS, Blocks.JUNGLE_PLANKS, Blocks.ACACIA_PLANKS, Blocks.DARK_OAK_PLANKS, Blocks.CRIMSON_PLANKS, Blocks.WARPED_PLANKS);
+        tag(BlockTags.LOGS)
+                .add(Blocks.OAK_LOG, Blocks.SPRUCE_LOG, Blocks.BIRCH_LOG, Blocks.JUNGLE_LOG, Blocks.ACACIA_LOG, Blocks.DARK_OAK_LOG, Blocks.CRIMSON_STEM, Blocks.WARPED_STEM);
+    }
+
+    /**
+     * @return Get the name of the tag file.
+     */
+    @Override
+    public String getName() {
+        return "birdhaustags";
     }
 }
