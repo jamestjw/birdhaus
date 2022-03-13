fac1a987e3103dddc718a783f3a62be02d644e11
ZackDaw <ZacharyJonesMail@gmail.com>
2022-03-13 19:40:33 -0400

    Updated the Recipe Generator to be able to still use tags rather than having to use ItemLikes.

--- src/main/java/com/ocelotslovebirds/birdhaus/datagen/RecipesGen.java
+++ src/main/java/com/ocelotslovebirds/birdhaus/datagen/RecipesGen.java
@@ -6,11 +6,15 @@ import java.util.function.Consumer;
 
 import com.ocelotslovebirds.birdhaus.setup.Registration;
 
+import net.minecraft.advancements.critereon.InventoryChangeTrigger;
+import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
-import net.minecraft.world.item.Items;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.ItemLike;
 
 /**
@@ -30,26 +34,28 @@ public class RecipesGen extends RecipeProvider {
      * @param planks         Ingredient for the birdhouse.
      */
     private static void birdhouseRecipe(
-            Consumer<FinishedRecipe> recipeConsumer, ItemLike birdhouse, ItemLike log, ItemLike planks) {
+            Consumer<FinishedRecipe> recipeConsumer, ItemLike birdhouse, TagKey<Item> log, TagKey<Item> planks) {
         //Note: To add more ingredients, overload this method with more ItemLikes, provide the item in the function call
         //and provide a pattern and define below. Unlocked by controls what unlocks the recipe for a player in survival
         // mode.
-
-        // Added tag item type - to enable spawing birdhouse using any type of plank OR logs
-        ShapedRecipeBuilder shaped = ShapedRecipeBuilder.shaped(birdhouse);
-        shaped.pattern("LLL");
-        shaped.pattern(" P ");
-        shaped.pattern(" P ");
-        shaped.define('L', log);
-        shaped.define('P', planks);
-        shaped.unlockedBy("has_log", has(log));
-        shaped.unlockedBy("has_planks", has(planks));
-        shaped.group("birdhouse");
-        shaped.save(recipeConsumer);
+        ShapedRecipeBuilder.shaped(birdhouse)
+                .pattern("LLL")
+                .pattern(" P ")
+                .pattern(" P ")
+                .define('L', log)
+                .define('P', planks)
+                .unlockedBy("has_log", has(log))
+                .unlockedBy("has_planks", has(planks))
+                .group("birdhouse")
+                .save(recipeConsumer);
     }
 
     @Override
     protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
-        birdhouseRecipe(consumer, Registration.BIRDHOUSE_BLOCK.get(), Items.OAK_LOG, Items.OAK_PLANKS);
+        birdhouseRecipe(consumer, Registration.BIRDHOUSE_BLOCK.get(), ItemTags.LOGS, ItemTags.PLANKS);
+    }
+
+    protected static InventoryChangeTrigger.TriggerInstance has(TagKey<Item> tagItem) {
+        return inventoryTrigger(ItemPredicate.Builder.item().of(tagItem).build());
     }
 }
